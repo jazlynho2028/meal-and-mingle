a05df1183d00d9bb8ff6eda2ad586ea22516f082
jazlynho2028 <jazlynho2028@u.northwestern.edu>
2024-11-10 10:50:14 -0600

    Created custom hook usePostList

--- src/common/posts/PostList.js
+++ src/common/posts/PostList.js
@@ -1,52 +1,13 @@
-import { useState, useEffect } from 'react';
-import User from '../users/User';
 import './Post.css';
 import Card from '../card/Card';
+import usePostList from './usePostList';
 
 // contains the displayed list of posts
 // isProfileList: true if list is the user's personal or saved lists
 // isUserSavedList: true if list is the user's saved list
-const PostList = ({header, Posts, isProfileList, isUserSavedList}) => {
-    const [posts, setPosts] = useState(Posts);
+const PostList = ({ header, Posts, isProfileList, isUserSavedList }) => {
   
-    const handleBookmark = (index) => {
-      const newPosts = [...posts];
-      newPosts[index].bookmarked = !newPosts[index].bookmarked;
-      
-      if (newPosts[index].bookmarked) {
-        User.saved.push(newPosts[index]);
-      }
-      else {
-        User.saved = User.saved.filter(post => post.id !== newPosts[index].id);
-      }
-  
-      setPosts(newPosts);
-    }
-  
-    const handleShow = (index) => {
-      const newPosts = [...posts];
-      newPosts[index].show = false;
-      
-      User.display = User.display.filter(post => post.id !== newPosts[index].id);
-      setPosts(newPosts.filter(post => post.id !== newPosts[index].id));
-    }
-  
-    // when save button is clicked
-    useEffect(() => {
-      Posts.forEach((post, index) => {
-        if (post.bookmarked) {
-          console.log('Post ' + index + ' is saved');
-        } 
-        else {
-          console.log('Post ' + index + ' is not saved');
-        }
-      })
-    }, [posts])
-  
-    // when x button is clicked
-    useEffect(() => {
-      console.log(`Current ${header}: `, posts);
-    },  [posts])
+  const { posts, handleBookmark, handleShow } = usePostList(header, Posts);
   
     return (
       <section className='postsSection'>
